114f200d270dd5340b6fd788d1f259d2d82a4337
Kevin <jingsi.chen@qq.com>
2017-08-31 17:02:40 +0800

    hfad: chapter9: action bars

---
+++ hf-android-dev/chapter9/BitsAndPizzas/app/src/main/java/com/eva/bitsandpizzas/MainActivity.java
@@ -0,0 +1,55 @@
+package com.eva.bitsandpizzas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+
+public class MainActivity extends Activity {
+
+  private ShareActionProvider shareActionProvider;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // inflate the menu; this adds items to the action bar if it is present
+    // menu: represents the action bar
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+
+    MenuItem menuItem = menu.findItem(R.id.action_share);
+    shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+    setIntent("This is example text");
+
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  // the MenuItem object is the item on the action bar that was clicked
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_create_order:
+        // This intent is used to start OrderActivity when the
+        // Create Order action item is clicked
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+        return true;
+      case R.id.action_settings:
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+  private void setIntent(String text) {
+    Intent intent = new Intent(Intent.ACTION_SEND);
+    intent.setType("text/plain");
+    intent.putExtra(Intent.EXTRA_TEXT, text);
+    shareActionProvider.setShareIntent(intent);
+  }
+}
